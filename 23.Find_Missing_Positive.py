0395b2db0040c806b6fe283a6ee3eb52527db22c
Swapnil Vishwakarma <swapnil.vishwakarma18@siesgst.ac.in>
2021-01-23 22:22:22 +0530

    Day23 Find Missing Positive

---
+++ 23.Find_Missing_Positive.py
@@ -0,0 +1,16 @@
+# Given an unsorted integer array nums, find the smallest missing positive integer.
+# Follow up: Could you implement an algorithm that runs in O(n) time and uses constant extra space.?
+
+class Solution:
+    def firstMissingPositive(self, nums: list) -> int:
+        ans = 1
+        if 1 not in nums:
+            return ans
+        for i in range(len(nums)):
+            if ans in nums:
+                ans += 1
+        
+        return ans
+
+sol = Solution()
+print(sol.firstMissingPositive([7,8,9,11,12]))
