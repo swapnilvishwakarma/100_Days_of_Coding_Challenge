3289f04648cddb34d1282f92c9a1101e2040bd45
Swapnil Vishwakarma <swapnil.vishwakarma18@siesgst.ac.in>
2021-04-02 08:07:52 +0530

    Day92 Maximum Sum BST in Binary Tree

---
+++ 92.Maximum_Sum_BST_in_Binary_Tree.py
@@ -0,0 +1,34 @@
+# Given a binary tree root, the task is to return the maximum sum of all keys of any sub-tree which is also a Binary Search Tree (BST).
+
+# Assume a BST is defined as follows:
+# The left subtree of a node contains only nodes with keys less than the node's key.
+# The right subtree of a node contains only nodes with keys greater than the node's key.
+# Both the left and right subtrees must also be binary search trees.
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def maxSumBST(self, root: TreeNode) -> int:
+        res = 0
+        def traverse(root):
+            '''return status_of_bst, size_of_bst, left_bound, right_bound'''
+            nonlocal res
+            if not root: return 1, 0, None, None # this subtree is empty
+            
+            ls, l, ll, lr = traverse(root.left)
+            rs, r, rl, rr = traverse(root.right)
+            
+            if ((ls == 2 and lr < root.val) or ls == 1) and ((rs == 2 and rl > root.val) or rs == 1):
+		        # this subtree is a BST
+                size = root.val + l + r
+                res = max(res, size)
+                return 2, size, (ll if ll is not None else root.val), (rr if rr is not None else root.val)
+            return 0, None, None, None # this subtree is not a BST
+        
+        traverse(root)
+        return res
